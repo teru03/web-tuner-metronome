77521b988d22f1ea6c1a815be177e2fc0acb1d1b
森本晃夫 <morimoto@multisoup.co.jp>
2025-06-15 13:35:39 +0900

    処理修正

--- js/metronome.js
+++ js/metronome.js
@@ -131,7 +131,7 @@ function draw() {
 }
 
 function init(){
-    var container = document.createElementByClassName( '.beatCanvas' );
+    var container = document.getElementByClassName( '.beatCanvas' );
 
     canvas = document.createElement( 'canvas' );
     canvasContext = canvas.getContext( '2d' );
