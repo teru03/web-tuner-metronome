d9c2614bda99437933da68c5f8274714111f060b
Teruo Morimoto <terusun@gmail.com>
2025-08-14 16:27:21 +0900

    ビルドエラー対応

--- next.config.js
+++ next.config.js
@@ -2,8 +2,14 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
 })
 
 module.exports = withPWA({
   reactStrictMode: true,
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 })
