534ca6537b8ac20e57d84b23b980de5fb82f7c9f
Teruo Morimoto <terusun@gmail.com>
2025-08-14 17:01:16 +0900

    config修正

--- next.config.js
+++ next.config.js
@@ -3,17 +3,12 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  // GitHub Pages では SW を無効化したいなら true にするなど
-  disable: process.env.GITHUB_ACTIONS === 'true',
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   output: 'export',
-  trailingSlash: true,
-  images: { unoptimized: true },
 };
 
 // ここで分岐しない（configure-pages が読みやすい形に）
-module.exports = withPWA(nextConfig);
+module.exports = withPWA(nextConfig,  {reactStrictMode: true});
