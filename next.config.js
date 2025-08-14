e8bda4672040688544488492687c95eabcc23f39
Teruo Morimoto <terusun@gmail.com>
2025-08-14 16:29:53 +0900

    ビルドエラー対応

--- next.config.js
+++ next.config.js
@@ -1,15 +1,22 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-})
+const isExport = process.env.NODE_ENV === 'production' && process.env.GITHUB_ACTIONS
 
-module.exports = withPWA({
+const nextConfig = {
   reactStrictMode: true,
   output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
-})
+}
+
+if (isExport) {
+  module.exports = nextConfig
+} else {
+  const withPWA = require('next-pwa')({
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development',
+  })
+  module.exports = withPWA(nextConfig)
+}
