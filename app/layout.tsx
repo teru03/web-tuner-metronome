69d0122a927832844a75fc76c0f8d3993569dfe3
Teruo Morimoto <terusun@gmail.com>
2025-08-19 22:48:38 +0900

    manifest.jsonの場所修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <head>
-        <link rel="manifest" href="/manifest.json" />
+        <link rel="manifest" href="web-tuner-metronome/manifest.json" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="Rhythm&Tune" />
